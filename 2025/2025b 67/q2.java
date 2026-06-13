fb935a5436b18957d7233e04e60e677cde4f4d2e
Yohai Dejorno <dejornoyohai@outlook.com>
2026-06-13 23:10:19 +0300

    s sdada 13.6

--- 2025/2025b 67/q2.java
+++ 2025/2025b 67/q2.java
@@ -1,36 +1,26 @@
-public class q2
-{
-    public static int findTriplet(int[] arr)
-    {
+public class q2 {
+    public static int findTriplet(int[] arr) {
         int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE;
         int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE, min3 = Integer.MAX_VALUE;
 
-        for (int i = 0; i < arr.length; i++)
-        {
-            if (arr[i] < min1)
-            {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min1) {
                 min3 = min2;
                 min2 = min1;
                 min1 = arr[i];
-            }
-            else if (arr[i] < min2)
-            {
+            } else if (arr[i] < min2) {
                 min3 = min2;
                 min2 = arr[i];
-            }
-            else if (arr[i] < min3)
+            } else if (arr[i] < min3)
                 min3 = arr[i];
 
-            if (arr[i] > max1)
-            {
+            if (arr[i] > max1) {
                 max2 = max1;
                 max1 = arr[i];
-            }
-            else if (arr[i] > max2)
+            } else if (arr[i] > max2)
                 max2 = arr[i];
         }
-        if (max1 * max2 * min1 < min1 * min2 * min3)
-        {
+        if (max1 * max2 * min1 < min1 * min2 * min3) {
             System.out.println(max1 + "\t" + max2 + "\t" + min1);
             return max1 * max2 * min1;
         }
